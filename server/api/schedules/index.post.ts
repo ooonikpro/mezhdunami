1480a41b6d460445c021625be8dbc5ae49ced354
Никита Овечкин <n.ovechkin.ccs@gmail.com>
2022-10-29 19:39:32 +0200

    feat: notify about new patient

--- server/api/schedules/index.post.ts
+++ server/api/schedules/index.post.ts
@@ -1,16 +1,25 @@
 import { addToSchedule } from "~~/db/collections/schedule";
-import { useTelegramBot } from "~~/composables/useTelegramBot";
+import { useCalendar } from '~~/composables/useCalendar';
+import { useProcedures } from '~~/composables/useProcedures';
+import { notifySubscribers } from '~~/services/telegramBot';
+
+const getNotificationText = (data: Tech.PatientFormData) => {
+    const { getLocalizedFullDate } = useCalendar();
+    const { getNames } = useProcedures();
+
+    return `<b>${getLocalizedFullDate(data.date)}</b>\nИмя: <b>${data.name}</b>\nТелефон: ${data.phone}\nПроцедуры: <b>${getNames(data.procedures)}</b>`;
+}
 
 export default defineEventHandler<Tech.ResponseAPI<boolean>>(async (event) => {
     try {
-        const data = await readBody(event);
+        const data = await readBody<Tech.PatientFormData>(event);
         const success = await addToSchedule(data);
 
         if (!success) {
             throw new Error('Это время уже занято. Пожалуйста, выберите другое');
         }
 
-        useTelegramBot();
+        notifySubscribers(getNotificationText(data))
 
         return {
             data: true,
---
+++ services/telegramBot.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const client = axios.create({
+    baseURL: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}`
+})
+
+const subscribers = ['209442952'];
+
+if (process.env.NODE_ENV === 'production') {
+    subscribers.push('460173300');
+}
+
+const sendMessage = (to: string, message: string) => client.get(`/sendMessage`, {
+    params: {
+        chat_id: to,
+        text: message,
+        parse_mode: 'HTML',
+        disable_notification: false
+    }
+});
+
+export const notifySubscribers = (message: string) => subscribers.map((to) => sendMessage(to, message));
