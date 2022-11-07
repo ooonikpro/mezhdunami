e7bd22d6d1b1d61c6100cd120fccc055567e73a1
Никита Овечкин <n.ovechkin.ccs@gmail.com>
2022-11-07 15:37:09 +0200

    refactor: remove old

--- old/composables/useBodyBg.ts
+++
@@ -1,8 +0,0 @@
-export const useBodyBg = () => {
-    return (color: string) => {
-        document
-            .querySelector('meta[name="theme-color"]')
-            .setAttribute("content", color);
-        document.documentElement.style.setProperty('--bg-color', color);
-    }
-}
--- old/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './telegramBot';
-export * from './smsAero';
