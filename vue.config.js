f44e719d821a9ae4996ffd170554dc315e2c1944
Никита Овечкин <n.ovechkin.ccs@gmail.com>
2022-11-11 22:45:33 +0200

    feat: work version

--- vue.config.js
+++ vue.config.js
@@ -18,4 +18,19 @@ module.exports = defineConfig({
       },
     },
   },
+
+  pwa: {
+    themeColor: '#F3BAB3',
+    msTileColor: '#F3BAB3',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    manifestOptions: {
+      name: 'Между Нами. Косметология для своих',
+      short_name: 'Между Нами',
+      display: 'standalone',
+      background_color: '#F3BAB3',
+      lang: 'ru',
+      dir: 'ltr',
+    },
+  },
 });
