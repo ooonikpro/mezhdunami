f237ac0215e50019698ed378f90ca5834f1512e0
Nikita Ovechkin <pumpingmonster04b63@gmail.com>
2022-11-11 18:32:13 +0200

    feat: new version

--- vue.config.js
+++ vue.config.js
@@ -15,13 +15,7 @@ module.exports = defineConfig({
 
   pwa: {
     workboxPluginMode: 'GenerateSW',
-    name: 'Косметология для своих',
-    themeColor: '#F3BAB3',
-    msTileColor: '#F3BAB3',
-    appleMobileWebAppStatusBarStyle: '',
-    manifestCrossorigin: 'anonymous',
-    manifestOptions: {
-      background_color: '#F3BAB3',
-    },
+    manifestPath: 'manifest.json',
+    iconPaths: null
   },
 });
